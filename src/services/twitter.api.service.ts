b4ff49b50f6b357083ec5a164f2f8f5f7ebfac09
Laqwan Brodie <lbrodie@gmail.com>
2019-10-26 13:18:25 -0400

    Initial commit for quotes bot

---
+++ src/services/twitter.api.service.ts
@@ -0,0 +1,82 @@
+import { Logger } from '@nestjs/common';
+import * as oauth from 'oauth';
+import qp from 'query-params';
+import '../config/env';
+
+const logger = Logger;
+
+const {
+  TWITTER_URL,
+  TWITTER_VERSION,
+  TWITTER_CONSUMER_KEY,
+  TWITTER_CONSUMER_SECRET,
+  TWITTER_TOKEN,
+  TWITTER_TOKEN_SECRET,
+} = process.env;
+
+const client = new oauth.OAuth(
+  `${TWITTER_URL}/oauth/request_token`,
+  `${TWITTER_URL}/oauth/access_token`,
+  TWITTER_CONSUMER_KEY,
+  TWITTER_CONSUMER_SECRET,
+  '1.0A',
+  null,
+  'HMAC-SHA1',
+);
+
+export const getPlaceId = async (query: any) => {
+  const places: any = await get('/geo/search', { query });
+  return places.result.places[0].id;
+};
+
+export const getTweets = userId =>
+  get('/statuses/user_timeline', { user_id: userId });
+
+export const tweet = status => post('/statuses/update', { status });
+
+export const tweetWithMedia = (status, mediaId) =>
+  post('/statuses/update', { status, media_ids: mediaId });
+
+export const deleteTweet = id => post(`/statuses/destroy/${id}`);
+
+export const uploadMedia = dataBuffer =>
+  upload(`/media/upload`, { media_data: dataBuffer.toString('base64') });
+
+const get = (url, params) =>
+  request(
+    'get',
+    `${TWITTER_URL}${TWITTER_VERSION}${url}.json?${qp.encode(params)}`,
+  );
+
+const post = (url, body?) =>
+  request('post', `${TWITTER_URL}${TWITTER_VERSION}${url}.json`, body);
+
+const upload = (url, body) =>
+  request('post', `${TWITTER_URL}${TWITTER_VERSION}${url}.json`, body, true);
+
+const request = (type, url, body?, isUpload?) => {
+  logger.debug(type.toUpperCase(), url, isUpload ? '' : body);
+  return new Promise((resolve, reject) => {
+    const callback = (err, res) => {
+      if (err) {
+        logger.error(
+          err.statusCode ? err.statusCode : err,
+          err.data ? err.data : '',
+        );
+        return reject(err);
+      } else {
+        logger.debug(`${type.toUpperCase()} OK - ${res.slice(0, 60)} ...`);
+        return resolve(JSON.parse(res));
+      }
+    };
+    const args = [url, TWITTER_TOKEN, TWITTER_TOKEN_SECRET];
+    if (type === 'post') {
+      args.push(body, 'application/x-www-form-urlencoded');
+    }
+    if (isUpload) {
+      args[0] = url.replace('api', 'upload');
+    }
+    args.push(callback);
+    client[type](...args);
+  });
+};
